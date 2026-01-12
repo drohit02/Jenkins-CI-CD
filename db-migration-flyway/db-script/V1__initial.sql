e6570b3625f031c22f6a82b3b81072163e49f916
Rohit-Deshmukh <drohit045@gmail.com>
2026-01-13 00:01:10 +0530

    Jenkinsfile updated

--- db-migration-flyway/db-script/V1__initial.sql
+++ db-migration-flyway/db-script/V1__initial.sql
@@ -6,5 +6,4 @@ CREATE TABLE users (
     username    VARCHAR(100) NOT NULL UNIQUE,
     email       VARCHAR(150) NOT NULL UNIQUE,
     created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    
 );
