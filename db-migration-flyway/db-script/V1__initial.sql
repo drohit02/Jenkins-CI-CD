8e4869609c48ede3978f12f1b93e09faaa18e35a
Rohit-Deshmukh <drohit045@gmail.com>
2026-01-12 23:17:23 +0530

    CI-CD code for the Migration

---
+++ db-migration-flyway/db-script/V1__initial.sql
@@ -0,0 +1,9 @@
+-- V1__init.sql
+-- Initial database setup
+
+CREATE TABLE users (
+    id          SERIAL PRIMARY KEY,
+    username    VARCHAR(100) NOT NULL UNIQUE,
+    email       VARCHAR(150) NOT NULL UNIQUE,
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
